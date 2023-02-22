0352f15f2af05dc2663fbd4c0c95b3274867437a
unknown <manojbk488@gmail.com>
2023-02-22 21:07:09 +0545

    added serving up static asset and added hbs for dynamic page

--- NodeJS Course/web-server/src/app.js
+++ NodeJS Course/web-server/src/app.js
@@ -1,27 +1,30 @@
+
+const path = require('path');
 const express = require('express');
+
+
+
+// console.log(__dirname);
+// // console.log(__filename);
+// console.log(path.join(__dirname, '../public'))
+
+
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+
 app.get('', (req, res) =>{
-    res.send('<h1>Weather</h1>');
+    res.render('index', {
+        'title': 'Weather',
+        'name': 'Manoj BK',
+    });
 });
 
-app.get('/help', (req, res) => {
-    res.send([
-        {
-            'name': 'Manoj',
-            'age': 24,
-        },
-        {
-            'name': 'Sara',
-        },
-        
-    ]);
-});
 
-app.get('/about', (req, res) =>{
-    res.send('<h1>About</h1>');
-});
 
 app.get('/weather', (req, res) => {
     res.send({
