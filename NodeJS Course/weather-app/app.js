de844457b9358b7a4419f5f6cbb4a83e7186783b
unknown <manojbk488@gmail.com>
2023-02-25 18:47:43 +0545

    added forecast call back function

--- NodeJS Course/weather-app/app.js
+++ NodeJS Course/weather-app/app.js
@@ -1,46 +1,13 @@
-const request = require('postman-request');
-const geocode = require('./utils/geocode')
-// const url ='http://api.weatherstack.com/current?access_key=dcc0349c8a5d890b23e36e096aebb40b&query=37.8267,-122.4233';
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-// request({url: url, json: true}, (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to weather service!');
-//     } else if (response.body.error){
-//         console.log('Unable to find location');
-
-//     }
-//     else {
-//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degree out. There is a ' + response.body.current.precip + ' % chance of rain.');
-
-//     }
-  
-// });
-
-//for geocoding
-
-
-
-
-// const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmttYW5vajEyMyIsImEiOiJjbGVqZHVyazIwYWozM3FrNWtha3V0dWd1In0.ON5NDAiHEB-U39tW3gfqKw';
-
-// request({uri: geoCodeUrl, json: true}, (error, response) => {
-//     if(error){
-//         console.log('Unable to connect to location service');
-
-//     } else if(response.body.features.length === 0){
-//         console.log('Unable to find location. Try another search');
-
-//     } else {
-//         const latitude = response.body.features[0].center[1];
-//         const longitude = response.body.features[0].center[0];
-
-//         console.log(latitude, longitude);
-//     }
-// });
-
-
-
+ 
 geocode('boston', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+});
+
+forecast(44.1545, -75.7088, (error, data)=> {
     console.log('Error', error);
     console.log('Data', data);
 });
---
+++ NodeJS Course/weather-app/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('postman-request');
+const forecast = (latitude, longitude, callback) => {
+    const url ='http://api.weatherstack.com/current?access_key=dcc0349c8a5d890b23e36e096aebb40b&query=' + latitude + ',' + longitude + '&units=f';
+
+    request({url: url, json: true}, (error, response) => {
+        if(error){
+            callback('Unable to connect to weather service!', undefined);
+
+        } else if(response.body.error){
+            callback('Unable to find location!', undefined);
+
+        } else {
+            
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degree out. There is a ' + response.body.current.precip + ' % chance of rain.');
+        }
+
+    });
+
+};
+
+module.exports = forecast;
+
