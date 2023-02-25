4f1f48daebc24568a76fef004a34028a941dee66
unknown <manojbk488@gmail.com>
2023-02-25 08:31:56 +0545

    pass string query on http header request

--- NodeJS Course/web-server/src/app.js
+++ NodeJS Course/web-server/src/app.js
@@ -47,10 +47,32 @@ app.get('/help', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            'error': 'You must provide an address!',
+        });
+
+    }
+    console.log(req.query.address);
     res.send({
         'forecast': 'sunny',
         'location': 'ktm',
+        'address': req.query.address,
 
+    });
+    
+});
+
+app.get('/products', (req, res) =>{
+
+    if(!req.query.search){
+        return res.send({
+            'error': 'You must provide a search term',
+        });
+    }
+    console.log(req.query.search);
+    res.send({
+        'products': [],
     });
 });
 
